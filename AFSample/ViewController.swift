3c5202a06c9990aff4a2e015bbe4d485c75fc1f2
nedim-hasovic <hasovicnedim@gmail.com>
2017-03-26 10:08:15 +0200

    Add files via upload

---
+++ AFSample/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  AFSample
+//
+//  Created by Nedim Hasovic on 01/07/16.
+//  Copyright © 2016 Nedim Hasovic. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class ViewController: UIViewController
+{
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        Alamofire.request(.GET, "htttps/http://codewithchris.com/code/afsample.json").responseJSON
+            { (response) -> Void in
+            
+            // Check if the result has a value
+            
+            if let JSON = response.result.value {
+                print(JSON)             }
+    }
+    }
+
+     override func didReceiveMemoryWarning()
+     {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
